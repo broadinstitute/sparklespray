386cbe79ef807050a398c0df77121ab767039a6a
Philip Montgomery <pmontgom@broadinstitute.org>
2017-06-14 09:44:10 -0400

    reformatted source

--- kubeque/kubesub.py
+++ kubeque/kubesub.py
@@ -6,44 +6,47 @@ import subprocess
 
 
 def execute_command(cmd, ignore_error=False, capture_stdout=False):
-  if capture_stdout:
-      assert not ignore_error
-      return subprocess.check_output(cmd)
-  else:
-      if ignore_error:
-        subprocess.call(cmd)
-      else:
-        subprocess.check_call(cmd)
+    if capture_stdout:
+        assert not ignore_error
+        return subprocess.check_output(cmd)
+    else:
+        if ignore_error:
+            subprocess.call(cmd)
+        else:
+            subprocess.check_call(cmd)
+
 
 def add_secret_mount(config, secret_name, mount_path):
-  if "volumes" not in config['spec']:
-    config['spec']['volumes'] = []
-  config['spec']['volumes'].append({
-    "name": "secret-volume",
-    "secret": {
-      "secretName": secret_name
-    }
-  })
-
-  for c in config['spec']['containers']:
-    if "volumeMounts" not in c:
-      c["volumeMounts"] = []
-    c["volumeMounts"].append({
-      "name": "secret-volume",
-      "readOnly": True,
-      "mountPath": mount_path        
+    if "volumes" not in config['spec']:
+        config['spec']['volumes'] = []
+    config['spec']['volumes'].append({
+        "name": "secret-volume",
+        "secret": {
+            "secretName": secret_name
+        }
     })
+
+    for c in config['spec']['containers']:
+        if "volumeMounts" not in c:
+            c["volumeMounts"] = []
+        c["volumeMounts"].append({
+            "name": "secret-volume",
+            "readOnly": True,
+            "mountPath": mount_path
+        })
+
 
 def _set_resource_limits(kub_job_def, cpu_request, mem_limit):
     containers = kub_job_def["spec"]["template"]["spec"]["containers"]
     for c in containers:
         c["resources"] = {
-          "requests": {"cpu": cpu_request, "memory": mem_limit},
-          "limits": {"memory": mem_limit},
-          }
+            "requests": {"cpu": cpu_request, "memory": mem_limit},
+            "limits": {"memory": mem_limit},
+        }
 
 
-def create_kube_job_spec(name, parallelism, image, command, environment_vars=[], secrets=[], cpu_request="1.0", mem_limit="100M"):
+def create_kube_job_spec(name, parallelism, image, command, environment_vars=[], secrets=[], cpu_request="1.0",
+                         mem_limit="100M"):
     assert isinstance(command, list)
     config = {"apiVersion": "batch/v1",
               "kind": "Job",
@@ -89,18 +92,22 @@ def create_kube_job_spec(name, parallelism, image, command, environment_vars=[],
 
     return json.dumps(config)
 
+
 def submit_job_spec(job_spec):
     with tempfile.NamedTemporaryFile("wt") as t:
-      t.write(job_spec)
-      t.flush()
-      cmd = ["kubectl", "create", "-f", t.name]
-      execute_command(cmd)
+        t.write(job_spec)
+        t.flush()
+        cmd = ["kubectl", "create", "-f", t.name]
+        execute_command(cmd)
+
 
 def _gcloud_cmd(args, config_name="kubeque"):
-    return ["gcloud", "--configuration="+config_name] + list(args)
+    return ["gcloud", "--configuration=" + config_name] + list(args)
+
 
 def _full_cluster_name(project_id, zone, cluster_name):
     return "gke_{}_{}_{}".format(project_id, zone, cluster_name)
+
 
 def setup_cluster_creds(project_id, zone, cluster_name, configuration):
     # setup credentials for kubectl
@@ -128,43 +135,46 @@ def setup_cluster_creds(project_id, zone, cluster_name, configuration):
 
 
 def start_cluster(cluster_name, machine_type, num_nodes):
-  execute_command(_gcloud_cmd(["container", "clusters", "create",
-    cluster_name,
-    "--machine-type", machine_type,
-    "--num-nodes", str(num_nodes),
-    "--scopes", "datastore,storage-full,https://www.googleapis.com/auth/pubsub"
-  ]))
+    execute_command(_gcloud_cmd(["container", "clusters", "create",
+                                 cluster_name,
+                                 "--machine-type", machine_type,
+                                 "--num-nodes", str(num_nodes),
+                                 "--scopes", "datastore,storage-full,https://www.googleapis.com/auth/pubsub"
+                                 ]))
 
 
 def stop_cluster(cluster_name):
-  execute_command(_gcloud_cmd(["container", "clusters", "delete",
-    cluster_name]))
+    execute_command(_gcloud_cmd(["container", "clusters", "delete",
+                                 cluster_name]))
+
 
 def delete_job(jobid):
-  cmd = ["kubectl", "delete", "jobs/{}".format(jobid)]
-  execute_command(cmd, ignore_error=True)
+    cmd = ["kubectl", "delete", "jobs/{}".format(jobid)]
+    execute_command(cmd, ignore_error=True)
+
 
 def stop_job(jobid):
-  cmd = ["kubectl", "scale", "--replicas=0", "jobs/{}".format(jobid)]
-  execute_command(cmd)
+    cmd = ["kubectl", "scale", "--replicas=0", "jobs/{}".format(jobid)]
+    execute_command(cmd)
+
 
 def add_node_pool(cluster_name, node_pool_name, machine_type, num_nodes,
                   min_nodes, max_nodes, is_autoscale, is_preemptable):
-
     cmd = _gcloud_cmd(["alpha", "container", "node-pools", "create",
-           node_pool_name,
-           "--cluster", cluster_name,
-           "--machine-type", machine_type,
-           "--num-nodes", str(num_nodes),
-           "--scopes", "datastore,storage-full,https://www.googleapis.com/auth/pubsub"
-           ])
+                       node_pool_name,
+                       "--cluster", cluster_name,
+                       "--machine-type", machine_type,
+                       "--num-nodes", str(num_nodes),
+                       "--scopes", "datastore,storage-full,https://www.googleapis.com/auth/pubsub"
+                       ])
     if is_preemptable:
         cmd += ["--preemptible"]
     if is_autoscale:
         assert min_nodes is not None
         assert max_nodes is not None
-        cmd += ["--enable-autoscaling", "--max-nodes="+str(max_nodes), "--min-nodes="+str(min_nodes)]
+        cmd += ["--enable-autoscaling", "--max-nodes=" + str(max_nodes), "--min-nodes=" + str(min_nodes)]
     execute_command(cmd)
+
 
 def resize_node_pool(cluster_name, node_pool_name, count, min, max, autoscale):
     cmd = _gcloud_cmd(['alpha', 'container', 'clusters', 'update', cluster_name])
@@ -192,9 +202,10 @@ def resize_node_pool(cluster_name, node_pool_name, count, min, max, autoscale):
         execute_command(cmd)
 
     if count is not None:
-        cmd = _gcloud_cmd([ 'alpha', 'container', 'clusters', 'resize', cluster_name, "--size={}".format(count)])
+        cmd = _gcloud_cmd(['alpha', 'container', 'clusters', 'resize', cluster_name, "--size={}".format(count)])
         append_node_pool(cmd)
         execute_command(cmd)
+
 
 def cluster_status(cluster_name):
     cmd = _gcloud_cmd(['alpha', 'container', 'clusters', 'describe', cluster_name])
@@ -224,34 +235,37 @@ def cluster_status(cluster_name):
         dict(nodePool=np['name'],
              targetSize=np['targetSize'],
              autoscaling=np['autoscaling']['enabled'],
-             max=np['autoscaling'].get('maxNodeCount',''),
-             min=np['autoscaling'].get('minNodeCount',''),
+             max=np['autoscaling'].get('maxNodeCount', ''),
+             min=np['autoscaling'].get('minNodeCount', ''),
              machineType=np['config']['machineType'],
              status=np['status'])
         for np in nodePools
-    ]
+        ]
     _printTable(nodePoolRows, ["nodePool", "targetSize", "autoscaling", "min", "max", "machineType", "status"])
+
 
 def _printTable(rows, header):
     def get_col_width(h):
-        widths = [ len(str(row[h])) for row in rows ]
+        widths = [len(str(row[h])) for row in rows]
         return max(widths)
 
-    colwidths = [ max([get_col_width(h), len(h)]) for h in header ]
-    format_string = " ".join(["{{:{}}}".format(w+3) for w in colwidths])
-    header_div = " ".join(["-"*(w+3) for w in colwidths])
+    colwidths = [max([get_col_width(h), len(h)]) for h in header]
+    format_string = " ".join(["{{:{}}}".format(w + 3) for w in colwidths])
+    header_div = " ".join(["-" * (w + 3) for w in colwidths])
 
     print(format_string.format(*header))
     print(header_div)
     for row in rows:
         print(format_string.format(*[str(row[h]) for h in header]))
 
+
 def rm_node_pool(cluster_name, node_pool_name):
     cmd = _gcloud_cmd(["container", "node-pools", "delete",
-           node_pool_name,
-           "--cluster", cluster_name,
-        ])
+                       node_pool_name,
+                       "--cluster", cluster_name,
+                       ])
     execute_command(cmd)
+
 
 def peek(project_id, zone, cluster_name, pod_name, lines):
     import pykube
@@ -263,7 +277,7 @@ def peek(project_id, zone, cluster_name, pod_name, lines):
     config.set_current_context(full_cluster_name)
     print("user={}".format(config.user))
     api = pykube.HTTPClient(config)
-    pods = list(pykube.Pod.objects(api).filter( field_selector={ "metadata.name": pod_name } ))
+    pods = list(pykube.Pod.objects(api).filter(field_selector={"metadata.name": pod_name}))
     if len(pods) > 1:
         print("{} pods had the name {}".format(len(pods), pod_name))
     elif len(pods) == 0:
@@ -271,4 +285,3 @@ def peek(project_id, zone, cluster_name, pod_name, lines):
     else:
         pod = pods[0]
         print(pod.logs(tail_lines=lines))
-
