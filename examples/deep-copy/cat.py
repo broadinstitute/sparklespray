3bb5552c8f3f748aea962cb7d9b99157b6963c1b
Philip Montgomery <pmontgom@broadinstitute.org>
2017-10-13 22:51:59 -0400

    Added test job with subdirs

---
+++ examples/deep-copy/cat.py
@@ -0,0 +1,15 @@
+import sys
+
+x = ""
+
+for fn in sys.args[1:]:
+    with open(fn, "rt") as fd:
+        x += fd.read()
+
+with open("output.txt", "wt") as fd:
+    fd.write(x)
+
+os.makedirs("outdir/2")
+with open("outdir/2/3.txt", "wt") as fd:
+    fd.write(x)
+
---
+++ examples/deep-copy/run-test.sh
@@ -0,0 +1,1 @@
+kubeque sub --fetch ../fetched --local python '^cat.py' '^dir1/dir1/file' '^dir2/file2'
