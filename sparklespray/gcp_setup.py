e4664eec8a340ee03560d835cf29d29c0de9c18e
Philip Montgomery <pmontgom@broadinstitute.org>
2018-09-20 13:51:02 -0400

    temporarily add owner role to service account

--- sparklespray/gcp_setup.py
+++ sparklespray/gcp_setup.py
@@ -3,11 +3,19 @@ import os
 from sparklespray.util import random_string
 import re
 import sys
+from google.cloud import datastore
+from google.api_core import exceptions
+from google.cloud.storage.client import Client as GSClient
+from sparklespray.config import SCOPES
+from google.oauth2 import service_account
+import google.api_core.exceptions
+import time
 
 services_to_add = [  # "storage.googleapis.com",
     "datastore.googleapis.com", "storage-component.googleapis.com", "genomics.googleapis.com", "pubsub.googleapis.com", "storage-api.googleapis.com", "compute.googleapis.com"]
 
-roles_to_add = ["roles/compute.admin",
+roles_to_add = ["roles/owner", # Eventually drop this
+                "roles/compute.admin",
                 "roles/datastore.user",
                 "roles/genomics.pipelinesRunner",
                 "roles/pubsub.editor",
@@ -48,6 +56,20 @@ def create_service_account(service_acct, project_id, key_path):
     gcloud(['iam', 'service-accounts', 'keys', 'create', key_path,
             '--iam-account', f"{service_acct}@{project_id}.iam.gserviceaccount.com"])
 
+    # TODO Add check for access google.api_core.exceptions.Forbidden
+#    print("Waiting for a minute for permissions to take effect...")
+#    time.sleep(60)
+
+
+def can_reach_datastore_api(project_id, key_path):
+    credentials = service_account.Credentials.from_service_account_file(
+        key_path, scopes=SCOPES)
+    client = datastore.Client(project_id, credentials=credentials)
+    try :
+        client.get(client.key("invalid", "invalid"))
+        return True
+    except exceptions.NotFound:
+        return False
 
 def setup_project(project_id, key_path, bucket_name):
     print("Enabling services for project {}...".format(project_id))
@@ -61,13 +83,18 @@ def setup_project(project_id, key_path, bucket_name):
         create_service_account(service_acct, project_id, key_path)
     else:
         print(
-            f"Not creating service account because key already exists at {key_path}. Delete this and rerun if you wish to create a new service account.")
+            f"Not creating service account because key already exists at {key_path} Delete this and rerun if you wish to create a new service account.")
+
     setup_bucket(project_id, key_path, bucket_name)
 
-from google.cloud.storage.client import Client as GSClient
-from sparklespray.config import SCOPES
-from google.oauth2 import service_account
-import google.api_core.exceptions
+    if not can_reach_datastore_api(project_id, key_path):
+        print("Go to https://console.cloud.google.com/datastore/setup?project={} to choose where to store your data will reside in and then set up will be complete. Select \"Cloud Datastore\" and then select a region close to you, and then \"Create database\".".format(project_id) )
+        input("Hit enter once you've completed the above: ")
+        print("checking datastore again..")
+        if can_reach_datastore_api(project_id, key_path):
+            print("Success!")
+
+
 
 def setup_bucket(project_id, service_account_key, bucket_name):
     credentials = service_account.Credentials.from_service_account_file(
@@ -75,14 +102,7 @@ def setup_bucket(project_id, service_account_key, bucket_name):
 
     client = GSClient(project_id, credentials)
     bucket = client.bucket(bucket_name)
-#    needs_create = True
-#    try:
     needs_create = not bucket.exists()
-#    except google.api_core.exceptions.Forbidden:
-#        pass
-    
+
     if needs_create:
         bucket.create()
-#        print("Creating bucket {}".format(bucket_name))
-#        gsutil(["mb", "-p", project_id, f"gs://{bucket_name}/"])
-        
