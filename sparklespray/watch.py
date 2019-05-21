d48f132dc5306ffccb3e714d84e8cf4567b29ef7
Philip Montgomery <pmontgom@broadinstitute.org>
2019-05-21 14:44:07 -0400

    fixed bug where changing the # of nodes would not update the number of preemptive nodes allowed, resulting in all new nodes to not be preemptive.

--- sparklespray/watch.py
+++ sparklespray/watch.py
@@ -7,6 +7,7 @@ from .logclient import LogMonitor, CommunicationError
 from typing import Callable
 from .cluster_service import ClusterState
 from .txtui import print_log_content
+
 # from google.gax.errors import RetryError
 
 
@@ -28,14 +29,15 @@ def add_watch_cmd(subparser):
     parser = subparser.add_parser("watch", help="Monitor the job")
     parser.set_defaults(func=watch_cmd)
     parser.add_argument("jobid")
-    parser.add_argument("--nodes", "-n", type=int,
-                        help="The target number of workers")
+    parser.add_argument("--nodes", "-n", type=int, help="The target number of workers")
     parser.add_argument(
-        "--loglive", help="tail the first running task we can find", action="store_true")
+        "--loglive", help="tail the first running task we can find", action="store_true"
+    )
 
 
 def watch_cmd(jq: JobQueue, io: IO, cluster: Cluster, args):
     from .main import _resolve_jobid
+
     jobid = _resolve_jobid(jq, args.jobid)
     watch(io, jq, jobid, cluster, target_nodes=args.nodes, loglive=args.loglive)
 
@@ -61,10 +63,13 @@ def _exception_guard(deferred_msg, reset=None):
 
 def print_error_lines(lines):
     from termcolor import colored, cprint
+
     for line in lines:
         print(colored(line, "red"))
 
+
 import datetime
+
 
 def flush_stdout_from_complete_task(jq, io, task_id, offset):
     task = jq.task_storage.get_task(task_id)
@@ -72,7 +77,7 @@ def flush_stdout_from_complete_task(jq, io, task_id, offset):
 
     attempts = 0
     while True:
-        rest_of_stdout = io.get_as_str(spec['stdout_url'], start=offset)
+        rest_of_stdout = io.get_as_str(spec["stdout_url"], start=offset)
         if rest_of_stdout is not None:
             break
         log.warning("Log doesn't appear to exist yet. Will try again...")
@@ -83,14 +88,24 @@ def flush_stdout_from_complete_task(jq, io, task_id, offset):
 
     print_log_content(datetime.datetime.now(), rest_of_stdout)
 
-def _watch(job_id: str, state: ClusterState, initial_poll_delay: float, max_poll_delay: float, loglive: bool, cluster: Cluster,
-           poll_cluster: Callable[[], None],
-           flush_stdout_from_complete_task : Callable[[str, int], None] = None):
+
+def _watch(
+    job_id: str,
+    state: ClusterState,
+    initial_poll_delay: float,
+    max_poll_delay: float,
+    loglive: bool,
+    cluster: Cluster,
+    poll_cluster: Callable[[], None],
+    flush_stdout_from_complete_task: Callable[[str, int], None] = None,
+):
     log_monitor = None
     prev_summary = None
 
     while True:
-        with _exception_guard(lambda: "summarizing status of job {} threw exception".format(job_id)):
+        with _exception_guard(
+            lambda: "summarizing status of job {} threw exception".format(job_id)
+        ):
             state.update()
 
         if state.is_done():
@@ -107,8 +122,9 @@ def _watch(job_id: str, state: ClusterState, initial_poll_delay: float, max_poll
             poll_delay = min(poll_delay * 1.5, max_poll_delay)
 
         if state.failed_node_req_count > 3:
-            log.error("%d node requests failed. Aborting...",
-                      state.failed_node_req_count)
+            log.error(
+                "%d node requests failed. Aborting...", state.failed_node_req_count
+            )
             break
 
         poll_cluster()
@@ -119,27 +135,45 @@ def _watch(job_id: str, state: ClusterState, initial_poll_delay: float, max_poll
                 if len(running) > 0:
                     task = running[0]
                     if task.monitor_address is not None:
-                        log.info("Obtained monitor address for task %s: %s",
-                                 task.task_id, task.monitor_address)
+                        log.info(
+                            "Obtained monitor address for task %s: %s",
+                            task.task_id,
+                            task.monitor_address,
+                        )
                         log_monitor = LogMonitor(
-                            cluster.client, task.monitor_address, task.task_id)
-                        print_log_content(None,
-                                          "[starting tail of log {}]".format(log_monitor.task_id), from_sparkles=True)
+                            cluster.client, task.monitor_address, task.task_id
+                        )
+                        print_log_content(
+                            None,
+                            "[starting tail of log {}]".format(log_monitor.task_id),
+                            from_sparkles=True,
+                        )
         else:
             if state.is_task_running(log_monitor.task_id):
                 with _exception_guard(lambda: "polling log file threw exception"):
                     try:
                         log_monitor.poll()
                     except CommunicationError as ex:
-                        log.warning("Got error polling log. shutting down log watch: {}".format(ex))
+                        log.warning(
+                            "Got error polling log. shutting down log watch: {}".format(
+                                ex
+                            )
+                        )
                         log_monitor.close()
                         log_monitor = None
 
             else:
-                print_log_content(None,
-                                  "[{} is no longer running, tail of log stopping]".format(log_monitor.task_id), from_sparkles=True)
+                print_log_content(
+                    None,
+                    "[{} is no longer running, tail of log stopping]".format(
+                        log_monitor.task_id
+                    ),
+                    from_sparkles=True,
+                )
                 if flush_stdout_from_complete_task:
-                    flush_stdout_from_complete_task(log_monitor.task_id, log_monitor.offset)
+                    flush_stdout_from_complete_task(
+                        log_monitor.task_id, log_monitor.offset
+                    )
                 log_monitor = None
 
         time.sleep(poll_delay)
@@ -154,7 +188,7 @@ import os
 
 def start_docker_process(job_spec_str: str, consume_exe: str, work_dir: str):
     job_spec = json.loads(job_spec_str)
-    actions = job_spec['pipeline']['actions']
+    actions = job_spec["pipeline"]["actions"]
     # action 0 is curl downloading consume
     consume_action = actions[1]
     docker_image = consume_action["imageUri"]
@@ -165,20 +199,24 @@ def start_docker_process(job_spec_str: str, consume_exe: str, work_dir: str):
 
     # TODO: Add options for google creds
     docker_options = [
-        "-v", os.path.expanduser("~/.config/gcloud") + ":/google-creds",
-        "-e", "GOOGLE_APPLICATION_CREDENTIALS=/google-creds/application_default_credentials.json",
-        "-v", f"{consume_exe}:/mnt/consume", "-v", f"{work_dir}:/mnt"]
+        "-v",
+        os.path.expanduser("~/.config/gcloud") + ":/google-creds",
+        "-e",
+        "GOOGLE_APPLICATION_CREDENTIALS=/google-creds/application_default_credentials.json",
+        "-v",
+        f"{consume_exe}:/mnt/consume",
+        "-v",
+        f"{work_dir}:/mnt",
+    ]
 
     for src_port, dst_port in docker_portmapping.items():
         docker_options.extend(["-p", f"{src_port}:{dst_port}"])
 
-    cmd = ["docker", "run"] + \
-        docker_options + [docker_image] + docker_command
+    cmd = ["docker", "run"] + docker_options + [docker_image] + docker_command
 
     with open("sparkles-docker.log", "a") as docker_log:
         docker_log.write("Executing: {}".format(cmd))
-        proc = subprocess.Popen(
-            cmd, stderr=subprocess.STDOUT, stdout=docker_log)
+        proc = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=docker_log)
 
     return proc
 
@@ -187,7 +225,14 @@ class DockerFailedException(Exception):
     pass
 
 
-def local_watch(job_id: str, consume_exe: str, work_dir: str, cluster: Cluster, initial_poll_delay=1.0, max_poll_delay=30.0):
+def local_watch(
+    job_id: str,
+    consume_exe: str,
+    work_dir: str,
+    cluster: Cluster,
+    initial_poll_delay=1.0,
+    max_poll_delay=30.0,
+):
     loglive = True
 
     job = cluster.job_store.get_job(job_id)
@@ -202,13 +247,21 @@ def local_watch(job_id: str, consume_exe: str, work_dir: str, cluster: Cluster, 
             raise DockerFailedException("Docker process prematurely died")
 
     try:
-        _watch(job_id, state, initial_poll_delay,
-               max_poll_delay, loglive, cluster, poll_cluster)
+        _watch(
+            job_id,
+            state,
+            initial_poll_delay,
+            max_poll_delay,
+            loglive,
+            cluster,
+            poll_cluster,
+        )
 
         failures = state.get_failed_task_count()
         successes = state.get_successful_task_count()
         txtui.user_print(
-            f"Job finished. {successes} tasks completed successfully, {failures} tasks failed")
+            f"Job finished. {successes} tasks completed successfully, {failures} tasks failed"
+        )
 
         successful_execution = failures == 0
 
@@ -223,7 +276,16 @@ def local_watch(job_id: str, consume_exe: str, work_dir: str, cluster: Cluster, 
     return successful_execution
 
 
-def watch(io: IO, jq: JobQueue, job_id: str, cluster: Cluster, target_nodes=None, initial_poll_delay=1.0, max_poll_delay=30.0, loglive=None):
+def watch(
+    io: IO,
+    jq: JobQueue,
+    job_id: str,
+    cluster: Cluster,
+    target_nodes=None,
+    initial_poll_delay=1.0,
+    max_poll_delay=30.0,
+    loglive=None,
+):
     job = jq.get_job(job_id)
     flush_stdout_calls = [0]
 
@@ -233,9 +295,20 @@ def watch(io: IO, jq: JobQueue, job_id: str, cluster: Cluster, target_nodes=None
 
     if target_nodes is None:
         target_nodes = job.target_node_count
+        max_preemptable_attempts = job.max_preemptable_attempts
+    else:
+        max_preemptable_attempts = target_nodes * 2
 
-    resize_cluster = ResizeCluster(target_nodes,
-                                   max_preemptable_attempts=job.max_preemptable_attempts)
+    log.info(
+        "targeting %s nodes. First %s nodes will be preemptive (from job: %s, %s)",
+        target_nodes,
+        max_preemptable_attempts,
+        job.target_node_count,
+        job.max_preemptable_attempts,
+    )
+    resize_cluster = ResizeCluster(
+        target_nodes, max_preemptable_attempts=max_preemptable_attempts
+    )
     get_preempted = GetPreempted()
 
     state = cluster.get_state(job_id)
@@ -243,7 +316,9 @@ def watch(io: IO, jq: JobQueue, job_id: str, cluster: Cluster, target_nodes=None
 
     check_attempts = 0
     while len(state.get_tasks()) == 0:
-        log.warning("Did not see any tasks for %s, sleeping and will check again...", job_id)
+        log.warning(
+            "Did not see any tasks for %s, sleeping and will check again...", job_id
+        )
         time.sleep(5)
         state.update()
         check_attempts += 1
@@ -254,33 +329,48 @@ def watch(io: IO, jq: JobQueue, job_id: str, cluster: Cluster, target_nodes=None
         loglive = True
 
     try:
+
         def poll_cluster():
             with _exception_guard(lambda: "restarting preempted nodes threw exception"):
                 task_ids = get_preempted(state)
                 if len(task_ids) > 0:
                     log.warning(
-                        "Resetting tasks which appear to have been preempted: %s", ", ".join(task_ids))
+                        "Resetting tasks which appear to have been preempted: %s",
+                        ", ".join(task_ids),
+                    )
                     for task_id in task_ids:
                         jq.reset_task(task_id)
 
             with _exception_guard(lambda: "rescaling cluster threw exception"):
                 resize_cluster(state, cluster.get_cluster_mod(job_id))
 
-        _watch(job_id, state, initial_poll_delay,
-               max_poll_delay, loglive, cluster, poll_cluster, flush_stdout_from_complete_task=flush_stdout)
+        _watch(
+            job_id,
+            state,
+            initial_poll_delay,
+            max_poll_delay,
+            loglive,
+            cluster,
+            poll_cluster,
+            flush_stdout_from_complete_task=flush_stdout,
+        )
 
         failures = state.get_failed_task_count()
         successes = state.get_successful_task_count()
         txtui.user_print(
-            f"Job finished. {successes} tasks completed successfully, {failures} tasks failed")
+            f"Job finished. {successes} tasks completed successfully, {failures} tasks failed"
+        )
 
         if failures > 0:
             log.warning(
-                "At least one task failed. Dumping stdout from one of the failures.")
+                "At least one task failed. Dumping stdout from one of the failures."
+            )
             failed_tasks = state.get_failed_tasks()
             flush_stdout(failed_tasks[0].task_id, 0)
 
-        elif flush_stdout_calls[0] == 0 and loglive: # if we want the logs and yet we've never written out a single log, pick one at random and write it out
+        elif (
+            flush_stdout_calls[0] == 0 and loglive
+        ):  # if we want the logs and yet we've never written out a single log, pick one at random and write it out
             log.info("Dumping arbitrary successful task stdout")
             successful_tasks = state.get_successful_tasks()
             flush_stdout(successful_tasks[0].task_id, 0)
