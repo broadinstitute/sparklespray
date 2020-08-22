446b4d066d54cef73b8c66e8c5da50da085cc8c8
Philip Montgomery <pmontgom@broadinstitute.org>
2020-08-21 23:28:36 -0400

    moved pipeline api test to use new 'life science' api

--- sparklespray/pipeline_api_test.py
+++ sparklespray/pipeline_api_test.py
@@ -1,15 +1,19 @@
 from apiclient.discovery import build
 import time
+import json
+from google.cloud.storage.client import Client
 
-service = build(
-    "genomics",
-    "v2alpha1",
-    #    credentials=credentials,
-    # cache_discovery=True,
-    # cache=DirCache(".sparkles-cache/services"),
-)
+project = "broad-achilles"
+zone = "us-central1-b"
+location = "us-central1"
+log_dest = "gs://broad-achilles-kubeque/test-kube/node-logs/test/test-job.txt"
 
-mounts = [{"disk": "ephemeralssd", "path": "/mnt/"}]
+
+service = build("lifesciences", "v2beta",)
+# resp = service.projects().locations().list(name=f"projects/{project}").execute()
+# print(resp)
+
+mounts = [{"disk": "ephemeralssd", "path": "/mnt/data"}]
 
 pipeline_def = {
     "pipeline": {
@@ -19,11 +23,16 @@ pipeline_def = {
                 "commands": [
                     "bash",
                     "-c",
-                    "mkdir -p /mnt/data/buckets/broad-achilles-kubeque && gcsfuse --foreground -o ro "
-                    "--stat-cache-ttl 24h --type-cache-ttl 24h --file-mode 755 --implicit-dirs broad-achilles-kubeque /mnt/data/buckets/broad-achilles-kubeque & "
-                    "sleep 10 && echo container1 && cat /mnt/data/buckets/broad-achilles-kubeque/test/cas/4eb38fa0e9778adcfa6bed3fc52cd0ce97ecc5d80ca06924a079ef22df93adea",
+                    "mkdir -p /mnt/data/buckets/genomics-public-data && "
+                    "gcsfuse --foreground -o ro --implicit-dirs genomics-public-data /mnt/data/buckets/genomics-public-data & "
+                    "sleep 10 && "
+                    "echo container1 && "
+                    "ls -l /mnt/data/buckets/genomics-public-data/1000-genomes/other/sample_info/sample_info.schema && "
+                    "md5sum /mnt/data/buckets/genomics-public-data/1000-genomes/other/sample_info/sample_info.schema && "
+                    "echo sleeping && sleep 10000",
                 ],
-                "flags": ["ENABLE_FUSE", "RUN_IN_BACKGROUND"],
+                "enableFuse": True,
+                "runInBackground": True,
                 "mounts": mounts,
             },
             {
@@ -31,24 +40,20 @@ pipeline_def = {
                 "commands": [
                     "bash",
                     "-c",
-                    "sleep 12 && echo container2 && cat /mnt/data/buckets/broad-achilles-kubeque/test/cas/4eb38fa0e9778adcfa6bed3fc52cd0ce97ecc5d80ca06924a079ef22df93adea",
+                    "sleep 12 && "
+                    "echo container2 && "
+                    "md5sum /mnt/data/buckets/genomics-public-data/1000-genomes/other/sample_info/sample_info.schema",
                 ],
                 "mounts": mounts,
             },
             {
                 "imageUri": "google/cloud-sdk:alpine",
-                "commands": [
-                    "gsutil",
-                    "cp",
-                    "/google/logs/output",
-                    "gs://broad-achilles-kubeque/test-kube/node-logs/test/test-job.txt",
-                ],
-                "flags": ["ALWAYS_RUN"],
+                "commands": ["gsutil", "cp", "/google/logs/output", log_dest,],
+                "alwaysRun": True,
             },
         ],
         "resources": {
-            "projectId": "broad-achilles",
-            "zones": ["us-central1-b"],
+            "zones": [zone],
             "virtualMachine": {
                 "machineType": "n1-standard-2",
                 "preemptible": True,
@@ -69,28 +74,52 @@ pipeline_def = {
 
 
 def get_op(operation_name):
-    request = service.projects().operations().get(name=operation_name)
+    request = service.projects().locations().operations().get(name=operation_name)
     return request.execute()
 
 
-operation = service.pipelines().run(body=pipeline_def).execute()
+operation = (
+    service.projects()
+    .locations()
+    .pipelines()
+    .run(parent=f"projects/{project}/locations/{location}", body=pipeline_def)
+    .execute()
+)
 operation_name = operation["name"]
 print(operation_name)
 
-import json
-
+# poll operation status
 last_events_len = 0
 while True:
     op = get_op(operation_name)
-    events = op["metadata"]["events"]
+    events = op["metadata"].get("events", [])
     if len(events) > last_events_len:
         new_events = events[: (len(events) - last_events_len)]
         new_events.reverse()
         for event in new_events:
             print(json.dumps(event, indent=2))
         last_events_len = len(events)
-    # print(op)
-    if op["done"]:
+    #    print(op)
+    if op.get("done", False):
         break
     print("sleeping...")
     time.sleep(5)
+
+
+client = Client()
+
+
+def get_as_str(path):
+    import re
+
+    m = re.match("^gs://([^/]+)/(.*)$", path)
+    assert m != None, "invalid remote path: {}".format(path)
+    bucket_name = m.group(1)
+    path = m.group(2)
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(path)
+    return blob.download_as_string().decode("utf8")
+
+
+print("------ output ------")
+print(get_as_str(log_dest))
