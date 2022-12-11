d19ad56e576bf8ac6c07e1cd090641128ad3b38a
Philip Montgomery <pmontgom@broadinstitute.org>
2022-12-10 21:02:34 -0500

    WIP

--- scripts/build-grpc.sh
+++ scripts/build-grpc.sh
@@ -3,5 +3,5 @@ set -ex
 protoc -I protobuf/sparklespray/sparklespray protobuf/sparklespray/sparklespray/pb.proto --go_out=go/src/github.com/broadinstitute/kubequeconsume/pb
 #protoc -I protobuf/sparklespray/sparklespray protobuf/sparklespray/sparklespray/pb.proto --go_out=plugins=grpc:go/src/github.com/broadinstitute/kubequeconsume/pb
 #protoc -I sparklespray/ sparklespray/pb.proto --python_out kubeque/grpc_client
-python -m grpc_tools.protoc -I protobuf/sparklespray --python_out=. --grpc_python_out=. 'protobuf/sparklespray/sparklespray/pb.proto'
+python -m grpc_tools.protoc -I protobuf/sparklespray --python_out=./cli --grpc_python_out=./cli 'protobuf/sparklespray/sparklespray/pb.proto'
 
