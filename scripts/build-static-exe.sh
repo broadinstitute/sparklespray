a9102706b93f23b985f9021fae16ddf91a2d9fd6
Philip Montgomery <pmontgom@broadinstitute.org>
2023-04-20 22:57:44 -0400

    Some cleanup to keep binaries out of repo

--- scripts/build-static-exe.sh
+++ scripts/build-static-exe.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
-
-set -ex
-
-ORIG_PWD=`pwd`
-
-cd go/src/github.com/broadinstitute/kubequeconsume
-CGO_ENABLED=0 GOOS=linux go build -ldflags "-s" -a -installsuffix cgo -o $ORIG_PWD/dest-bin/kubequeconsume cli/main.go
-
-#docker run --rm -v $PWD/go:/go2 \
-#    -e GOPATH=/go:/go2 \
-#    -v $PWD/sparklespray/bin:/dest-bin \
-#    -w /dest-bin golang:1.11 \
-#    bash -c 'CGO_ENABLED=0 GOOS=linux go build -ldflags "-s" -a -installsuffix cgo -o /dest-bin/kubequeconsume github.com/broadinstitute/kubequeconsume/cli'
+PWD=`pwd`
+docker run --rm -v $PWD/go:/go2 \
+    -e GOPATH=/go:/go2 \
+    --platform linux/amd64 \
+    -v $PWD/cli/sparklespray/bin:/dest-bin \
+    -w /dest-bin golang:1.11 \
+    bash -c 'CGO_ENABLED=0 GOOS=linux go build -ldflags "-s" -a -installsuffix cgo -o /dest-bin/kubequeconsume github.com/broadinstitute/kubequeconsume/cli'
 
 
