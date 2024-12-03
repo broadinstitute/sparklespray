81dfb9ad4711822d89c81d1e09dc1b3434b4c7f1
Philip Montgomery <pmontgom@broadinstitute.org>
2024-12-02 21:39:33 -0500

    fixes to build scripts

--- scripts/build-static-exe.sh
+++ scripts/build-static-exe.sh
@@ -1,7 +1,7 @@
-cd go/src/github.com/broadinstitute/kubequeconsume 
+cd go/src/github.com/broadinstitute/sparklesworker 
 # build staticly linked amd64 executable
 GOARCH=amd64 CGO_ENABLED=0 GOOS=linux go build \
   -ldflags "-s" -a \
-  -installsuffix cgo -o ../../../../../cli/sparklespray/bin/kubequeconsume github.com/broadinstitute/kubequeconsume/cli
+  -installsuffix cgo -o ../../../../../cli/sparklespray/bin/sparklesworker github.com/broadinstitute/sparklesworker/cli
 
 
