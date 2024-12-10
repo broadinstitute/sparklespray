bb95e8c562cec6cf73df986f51f23ea9d6903e56
Philip Montgomery <pmontgom@broadinstitute.org>
2024-12-09 22:29:14 -0500

    Changed local build to not worry about making a static binary

--- go/local-build.sh
+++ go/local-build.sh
@@ -1,3 +1,3 @@
 DEST=`pwd`
 cd src/github.com/broadinstitute/sparklesworker/cli
-GCO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-linkmode 'external' -extldflags '-static'" -o ${DEST}/sparklesworker .
+go build -o ${DEST}/sparklesworker.local .
