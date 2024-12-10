1a4be237f49bf3dcd5e2969363543c9335c8e011
Philip Montgomery <pmontgom@broadinstitute.org>
2024-12-09 22:28:47 -0500

    updated build script to get a local copy of executable

--- go/build.sh
+++ go/build.sh
@@ -2,3 +2,7 @@ IMAGE_NAME=us-central1-docker.pkg.dev/cds-docker-containers/docker/sparklesworke
 docker build . -t sparklesworker
 docker tag sparklesworker ${IMAGE_NAME}
 docker push ${IMAGE_NAME}
+id=$(docker create ${IMAGE_NAME})
+docker cp $id:/sparklesworker - > sparklesworker.tar
+docker rm -v $id
+
