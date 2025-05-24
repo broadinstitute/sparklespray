54372b6f143af948947f72c513da55fc13f23394
Philip Montgomery <pmontgom@broadinstitute.org>
2025-05-24 02:35:08 +0000

    Shutdown if there are no nodes scheduled to be started

--- cli/sparklespray/watch/completion.py
+++ cli/sparklespray/watch/completion.py
@@ -1,11 +1,12 @@
 from .runner_types import PeriodicTask, NextPoll, StopPolling
-from .shared import _count_incomplete_tasks
+from .shared import _count_incomplete_tasks, _count_active_nodes
 
 
 class CompletionMonitor(PeriodicTask):
     def __init__(self):
         self.cur_delay = self.initial_delay = 0.5
         self.max_delay = 20
+        self.no_active_nodes_count = 0
 
     def next_poll_delay(self):
         self.cur_delay = min(self.max_delay, self.cur_delay * 2)
@@ -14,6 +15,11 @@ class CompletionMonitor(PeriodicTask):
     def poll(self, state):
         # Is everything done? If so, terminate loop.
         tasks = state.get_tasks()
+        nodes = state.get_nodes()
+        if _count_active_nodes(nodes) == 0:
+            self.no_active_nodes_count += 1
+            if self.no_active_nodes_count > 10:
+                return StopPolling()
         if _count_incomplete_tasks(tasks) == 0:
             return StopPolling()
         return self.next_poll_delay()
